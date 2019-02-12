a40b7c75588530b0f723507c719e3cec0488031d
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-11 19:23:26 -0800

    Add and implement passport strategy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
- const mongoose = require('mongoose');
-  const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
  // routes
  const users = require('./routes/api/users');
  const profile = require('./routes/api/profile');
@@ -19,7 +20,12 @@ mongoose.connect(db)
 .then(()=> console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
- app.get('/', (req,res) => res.send('Hello!'));
+//  app.get('/', (req,res) => res.send('Hello!'));
+//Passport middleware
+app.use(passport.initialize())
+
+// Passport config
+require('./config/passport')(passport);
 
  //use routes
  app.use('/api/users', users);
