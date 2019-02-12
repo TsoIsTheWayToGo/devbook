90c7d3c95e60413ff839e053a115db61d4d12f72
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-11 16:26:04 -0800

    Add routes to server.js and users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
+router.get('/test', (req, res) => res.json({msg: "Users Works"}));// will automattically do 200
+
+module.exports = router;
