8b63bb734e07f1c0cc1614000f16992cb9084fde
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-15 13:43:31 -0800

    Add post validations

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,4 +6,23 @@ const router = express.Router();
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
+
+
+
+// @route   POST api/posts/test
+// @desc    Create Post
+// @access  private
+
+router.post('/', passport.authenticate('jwt', {session: false})), (req, res => {
+  const newPost = new Post ({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.name,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post));
+});
+
+
 module.exports = router;
