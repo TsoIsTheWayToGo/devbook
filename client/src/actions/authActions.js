3f13b43f60bf82552c9d658b3e7a25a057bad520
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-20 12:11:16 -0800

    Add componentWillReceiveProps()

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -20,7 +20,7 @@ export const registerUser = (userData, history) => dispatch => {
 
 //Login - Get User token
 
-export const loginUsers = userData => dispatch => {
+export const loginUser = userData => dispatch => {
   axios.post('/api/users/login', userData)
     .then(res => {
       //Save to local storage
