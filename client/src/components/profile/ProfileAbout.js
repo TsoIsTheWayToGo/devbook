085232ed4957d40ab4813f4134b18d7cfd059751
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-21 14:51:34 -0800

    Made sure redux part is done

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class ProfileAbout extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TODO: Profile about</h1>
+      </div>
+    )
+  }
+}
+export default ProfileAbout;
