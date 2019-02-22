dce28bdd09d076fa2be1b255350477290ae697cb
TsoIsTheWayToGo <ericgordontso@gmail.com>
2019-02-22 13:39:26 -0800

    Add Get Post

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 import {
   ADD_POST,
-  GET_ERRORS
+  GET_ERRORS,
+  CLEAR_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING,
+  DELETE_POST
 } from './types';
-
 
 // Add Post
 export const addPost = postData => dispatch => {
@@ -24,3 +28,28 @@ export const addPost = postData => dispatch => {
       })
     );
 };
+// Get Post
+export const getPosts = () => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get('/api/posts')
+    .then(res =>
+      dispatch({
+        type: GET_POSTS,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+//set loading state
+export const setPostState = () => {
+  return {
+    type: POST_LOADING
+  }
+}
